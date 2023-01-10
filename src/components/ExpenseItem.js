5930447b2e75756a27c035bd47087e3a74267d57
Kermo <kermo.ohno@voco.com>
2023-01-10 11:41:41 +0200

    Split first custom component into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import './ExpenseItem.css'
 
-function ExpenseItem(){
-    return(
-    <div className="expense-item">
-        <div>Date</div>
-        <div className="expense-item__description">
-            <h2>Title</h2>
-            <div className="expense-item__price">Amount</div>
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props){
+
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}€</div>
+            </div>
         </div>
-    </div>
     )
 }
 
