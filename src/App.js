7ee9600a0380696f6f69f8f18d266a42b6ef5ab4
Kermo <kermo.ohno@voco.com>
2023-01-18 20:02:43 +0200

    Bottom up 2 push

--- src/App.js
+++ src/App.js
@@ -17,9 +17,14 @@ const App = () => {
         }
     ]
 
+    const addExpensehandler = (expense) => {
+        console.log('In app js')
+        console.log(expense)
+    }
+
     return (
         <div className="App">
-            <NewExpense></NewExpense>
+            <NewExpense onAddExpense={addExpensehandler}></NewExpense>
             <Expenses expenses={expenses}></Expenses>
         </div>
     );
