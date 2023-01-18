5c76b166dc5d2d8f77cd2e94d5474c2cc7c8cb9a
Kermo <kermo.ohno@voco.com>
2023-01-18 09:08:58 +0200

    Kuup2eva andmete kuvamise osa eraldamine

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,6 +2,9 @@ import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props){
+    const day = props.expenseData.date.toLocaleString('en-US', {day:'2-digit'})
+    const month = props.expenseData.date.toLocaleString('en-US', {month:'long'})
+    const year = props.expenseData.date.getFullYear()
 
     return (
         <div className="expense-item">
