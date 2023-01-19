654b31b833a967b42946d34be15a3a78c4c4c9e0
Kermo <kermo.ohno@voco.com>
2023-01-19 12:34:36 +0200

    Filtered system but not ready

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,27 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "../Expenses/ExpensesFilter"
+import {useState} from "react";
 
 const Expenses = (props) =>{
+    const [filteredYear, setFilteredYear] = useState('2023');
+
+    console.log('Year data in Expenses.js' + filteredYear)
+
+    const filterChangeHandler = (year) => {
+        console.log('Filter change handled by Expenses.js')
+        console.log(year + ' in Expenses.js')
+        setFilteredYear(year)
+    }
+
+    props.expenses.map((expense) => {
+        console.log(expense)
+    })
+
     return(
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
             <ExpenseItem expenseData={props.expenses[0]}></ExpenseItem>
             <ExpenseItem expenseData={props.expenses[1]}></ExpenseItem>
         </Card>
